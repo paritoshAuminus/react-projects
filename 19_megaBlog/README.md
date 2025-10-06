24c4be7953b205249c14343e4db9cea21dffe680
paritoshAuminus <paritoshverma2038@gmail.com>
2025-10-06 16:09:08 +0530

    Appwrite setup and env configuration

---
+++ 19_megaBlog/README.md
@@ -0,0 +1,9 @@
+# Using Appwrite
+
+Here we use <b>Appwrite</b> as a backend service, it provides us with multiple
+services like `Authentication`, `database` and `downloading`/`uploading` services, for that we install Appwrite to the project using documentation and create a project on the cloud which would act as our backend
+
+# .env file
+
+A very important file, for more information on it please visit the `.env` file stored
+inside the main app folder `./.env` and `./.env.sample`
---
+++ 19_megaBlog/src/config/config.js
@@ -0,0 +1,18 @@
+/*
+This file is just exporting all the code to access the environment variables,
+this serves two purposes:
+1. It enhances the readability of the code, we don't have to write import.meta.env.VITE_SOME_SHIT
+every time
+2. Sometimes the Ids in the database are made entirely of numbers (eg: '1223423412') which can be
+coerced as a number instead of a string causing the app to crash
+*/
+
+const config = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID)
+}
+
+export default config
